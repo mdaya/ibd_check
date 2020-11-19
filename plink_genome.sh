e6a5d3a9e0980dc9234b1e2703fff03804703c98
dayam <dayam@Xs-MacBook-Air.local>
2020-11-19 09:41:50 -0700

    Fixed input file name

--- plink_genome.sh
+++ plink_genome.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-plink_in_file=$1
+plink_bed_file=$1
 window_size=$2
 step_size=$3
 vif_threshold=$4
+
+plink_in_file=`sed 's/.bed//' $plink_bed_file`
 
 plink --bfile $plink_in_file \
    --indep $window_size $step_size $vif_threshold 
